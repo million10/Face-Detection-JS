aaf339f4d0bc34815bbdd42594adde6c861c6e2b
Million Fekadu Tekele <50737538+million10@users.noreply.github.com>
2025-02-05 10:41:54 -0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 
 <?php 
 
-$conn= new mysqli('localhost','root','','performance_eval')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root',''645790qwe@134,'performance_eval')or die("Could not connect to mysql".mysqli_error($con));
 $conn->set_charset("utf8mb4");
 ?>
